3b458acedf11b647e79f700ff07800f49090726a
Ilya Lunev <ilunev@protonmail.com>
2021-03-08 18:58:00 +0300

    Добавил обёртку для подсчёта числа вызовов функции

--- Lab01.py
+++ Lab01.py
@@ -12,13 +12,31 @@ def minimizer(fn):
     return fn
 
 
+class CallCounter:
+    """Обёртка для подсчёта числа вызовов функции"""
+
+    def __init__(self, fn):
+        self._fn = fn
+        self._count = 0
+
+    def __call__(self, *args, **kwargs):
+        self._count += 1
+        return self._fn(*args, **kwargs)
+
+    def get_count(self):
+        return self._count
+
+    def reset(self):
+        self._count = 0
+
+
 def f(x):
     """Исходная функция"""
     return math.sin(x) - math.log(x ** 2) - 1
 
 
 @minimizer
-def dichotomy_method(a0, b0, eps):
+def dichotomy_method(f, a0, b0, eps):
     """Метод дихотомии"""
     a = a0
     b = b0
@@ -26,14 +44,15 @@ def dichotomy_method(a0, b0, eps):
 
     interval_length = abs(b - a)
     algo_iters = 0
-    func_calls = 0
 
     while interval_length > eps:
         x1 = (a + b - delta) / 2
         x2 = (a + b + delta) / 2
-        if f(x1) > f(x2):
+        y1 = f(x1)
+        y2 = f(x2)
+        if y1 > y2:
             a = x1
-        elif f(x1) < f(x2):
+        elif y1 < y2:
             b = x2
         else:
             a = x1
@@ -41,14 +60,13 @@ def dichotomy_method(a0, b0, eps):
 
         interval_length = abs(b - a)
         algo_iters += 1
-        func_calls += 2
 
-    return (a + b) / 2.0, algo_iters, func_calls
+    return (a + b) / 2.0, algo_iters
 
 
 # TODO Метод золотого сечения - пока в разработке
 @minimizer
-def golden_ratio_method(a0, b0, eps):
+def golden_ratio_method(f, a0, b0, eps):
     """Метод золотого сечения"""
     a = a0
     b = b0
@@ -61,7 +79,6 @@ def golden_ratio_method(a0, b0, eps):
     y2 = f(x2)
 
     algo_iters = 0
-    func_calls = 2
 
     while interval_length > eps:
         if y1 >= y2:
@@ -70,17 +87,15 @@ def golden_ratio_method(a0, b0, eps):
             x2 = a + phi * (b - a)
             y1 = y2
             y2 = f(x2)
-            func_calls += 1
         else:
             b = x2
             x2 = x1
             x1 = b - phi * (b - a)
             y2 = y1
             y1 = f(x1)
-            func_calls += 1
         interval_length = b - a
         algo_iters += 1
-    return (a + b) / 2.0, func_calls, algo_iters
+    return (a + b) / 2.0, algo_iters
 
 
 if __name__ == "__main__":
@@ -88,12 +103,13 @@ if __name__ == "__main__":
     b0 = 7.0
     eps = 0.01
 
-    for minimizer in minimizers:
-        res, call_count, iter_count = minimizer(a0, b0, eps)
+    for algo in minimizers:
+        fn_counted = CallCounter(f)
+        res, iter_count = algo(fn_counted, a0, b0, eps)
         print(
-            f"Метод: {minimizer.__name__}",
+            f"Метод: {algo.__name__}",
             f"Результат: {res:.3f}",
-            f"Вызовов функции: {call_count}",
+            f"Вызовов функции: {fn_counted.get_count()}",
             f"Итераций: {iter_count}",
             sep="\n",
             end="\n\n",

b8ccc7029e804aef84ff29565765b284b022a18f
Ilya Lunev <ilunev@protonmail.com>
2021-03-24 00:22:20 +0300

    Отредактировал второй метод парабол

--- lab1/alg.py
+++ lab1/alg.py
@@ -132,25 +132,28 @@ def fibonacci_method(f, a, b, eps):
 
     return intervals
 
-def get_xs(f, x1, step, a0, b0):
 
-    if (x1 < a0) or (x1 > b0):
-        x1 = (a0 + b0) / 2
+def get_xs(f, x1, f1, step, a0, b0):
+    """Найти точки вблизи исследуемой. Точки возвращаются в естественном порядке"""
+
+    assert a0 <= x1 <= b0
 
     x2 = x1 + step
     x2 = max(a0, min(b0, x2))
-
-    f1 = f(x1)
     f2 = f(x2)
 
     if f1 > f2:
         x3 = x1 + 2 * step
     else:
         x3 = x1 - step
-    
+
     x3 = max(a0, min(b0, x3))
-    
-    return x2, x3, f1, f2
+    f3 = f(x3)
+
+    (x1, f1), (x2, f2), (x3, f3) = sorted([(x1, f1), (x2, f2), (x3, f3)])
+
+    return x1, x2, x3, f1, f2, f3
+
 
 def get_min_x_f(f1, f2, f3, x1, x2, x3):
     """Выбрать точку с минимальным значением функции"""
@@ -160,24 +163,25 @@ def get_min_x_f(f1, f2, f3, x1, x2, x3):
 
 
 def square_approximation(f, f1, f2, f3, x1, x2, x3, step, a0, b0):
-
-    #f1 = f(x1)
-    #f2 = f(x2)
-    #f3 = f(x3)
-
-    x_min, f_min = get_min_x_f(f1, f2, f3, x1, x2, x3)
+    """Найти вершину параболы по трём точкам. При линейном расположении точек перейти к новым"""
 
     if (x2 - x1) * (f2 - f3) - (x2 - x3) * (f2 - f1) == 0:
-        x_1, x_2, x_3 = get_xs(f, x_min, step, a0, b0)
-        return square_approximation(f, f1, f2, f3, x_1, x_2, x_3, step, a0, b0)
+        x_min, f_min = get_min_x_f(f1, f2, f3, x1, x2, x3)
+        x1, x2, x3, f1, f2, f3 = get_xs(f, x_min, f_min, step, a0, b0)
+        return square_approximation(f, f1, f2, f3, x1, x2, x3, step, a0, b0)
 
     else:
-        u = 0.5 * ((x2 ** 2 - x3 ** 2) * f1 + (x3 ** 2 - x1 ** 2) * f2 + (x1 ** 2 - x2 ** 2) * f3) / (
-            (x2 - x3) * f1 + (x3 - x1) * f2 + (x1 - x2) * f3
+        u = (
+            0.5
+            * (
+                (x2 ** 2 - x3 ** 2) * f1
+                + (x3 ** 2 - x1 ** 2) * f2
+                + (x1 ** 2 - x2 ** 2) * f3
+            )
+            / ((x2 - x3) * f1 + (x3 - x1) * f2 + (x1 - x2) * f3)
         )
         fu = f(u)
         return u, fu
-
 
 
 @minimizer
@@ -187,10 +191,11 @@ def parabola_method2(f, a0, b0, eps):
     eps /= 2
     intervals = []
     intervals.append((a0, b0))
-    step = 0.05  # min(0.05, max(abs(b0 - a0) * eps, 10 * eps))
+    step = 0.05
 
     x1 = (a0 + b0) / 2
-    x2, x3, f1, f2 = get_xs(f, x1, step, a0, b0)
+    f1 = f(x1)
+    x1, x2, x3, f1, f2, f3 = get_xs(f, x1, f1, step, a0, b0)
     f3 = f(x3)
 
     while True:
@@ -198,42 +203,26 @@ def parabola_method2(f, a0, b0, eps):
         x_min, f_min = get_min_x_f(f1, f2, f3, x1, x2, x3)
         u, fu = square_approximation(f, f1, f2, f3, x1, x2, x3, step, a0, b0)
 
-        # if (abs(x3 - x1) < eps / 2):
-        #     intervals.append((x1 - eps, x3 + eps))
-        #     break
-
-        # if ((abs(u - x_min) < eps) and (abs(x3 - x1) < eps)):
-        #     intervals.append((u - eps, u + eps))
-        #     break
-
         if (abs((f_min - fu) / fu) < eps) and (abs((x_min - u) / u) < eps):
             intervals.append((u - eps, u + eps))
             break
 
-        else:
-            if (u >= x1) and (u <= x3):
-                if (f_min + eps < fu) and (x_min < x2):
-                    x2 = x_min
-                else:
-                    x2 = u
-
-                assert((x2 >= a0) and (x2 <= b0))
-                x1 = max(a0, x2 - step)
-                x3 = min(b0, x2 + step)
-                f1 = f(x1)
-                f2 = f(x2)
-                f3 = f(x3)
-
+        if x1 <= u <= x3:
+            if (f_min + eps < fu):
+                x2, f2 = x_min, f_min
             else:
-                x1 = u
-                # if (u >= a0) and (u <= b0):
-                #     x1 = u
-                # else:
-                #     x1 = x_min
-                x2, x3, f1, f2 = get_xs(f, x1, step, a0, b0)
-                f3 = f(x3)
+                x2, f2 = u, fu
 
-            intervals.append((x1, x3))
+            assert (x2 >= a0) and (x2 <= b0)
+            x1 = max(a0, x2 - step)
+            x3 = min(b0, x2 + step)
+            f1 = f(x1)
+            f3 = f(x3)
+
+        else:
+            x1, x2, x3, f1, f2, f3 = get_xs(f, u, fu, step, a0, b0)
+
+        intervals.append((x1, x3))
 
     return intervals
 

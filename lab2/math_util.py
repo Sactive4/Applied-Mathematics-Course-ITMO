3bc0adca02ee7eb82000ff474831cb602bbf6b18
Ilya Lunev <ilunev@protonmail.com>
2021-04-15 10:06:05 +0300

    Реализовал градиент

--- lab2/math_util.py
+++ lab2/math_util.py
@@ -14,3 +14,17 @@ def partial_derivative(f, x, i):
     f_x1 = f(*x1)
 
     return (f_x1 - f_x) / (x1[i] - x[i])
+
+
+def gradient(f, x):
+    """Найти градиент функции f в точке x
+    f - дифференцируемая функция
+    x - вектор координат точки, len(x) совпадает с числом параметров f
+    """
+
+    result = x.copy()  # для сохранения типа и размерности вектора
+
+    for i in range(len(x)):
+        result[i] = partial_derivative(f, x, i)
+
+    return result
--- lab2/test_lab2.py
+++ lab2/test_lab2.py
@@ -2,27 +2,27 @@ from math import isclose
 
 import numpy as np
 
-from math_util import DIFFERENTIATION_STEP, partial_derivative
+from math_util import DIFFERENTIATION_STEP, gradient, partial_derivative
 
 
 def test_partial_derivative_hyperbola():
     fn = lambda x: x ** 3
 
     assert isclose(
-        partial_derivative(fn, [-2.], 0),
-        12.,
+        partial_derivative(fn, [-2.0], 0),
+        12.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
     assert isclose(
-        partial_derivative(fn, [2.], 0),
-        12.,
+        partial_derivative(fn, [2.0], 0),
+        12.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
     assert isclose(
-        partial_derivative(fn, [0.], 0),
-        0.,
+        partial_derivative(fn, [0.0], 0),
+        0.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
@@ -31,20 +31,20 @@ def test_partial_derivative_paraboloid():
     fn = lambda x, y: x ** 2 + y ** 2
 
     assert isclose(
-        partial_derivative(fn, [1., 2.], 0),
-        2.,
+        partial_derivative(fn, [1.0, 2.0], 0),
+        2.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
     assert isclose(
-        partial_derivative(fn, [1., 2.], 1),
-        4.,
+        partial_derivative(fn, [1.0, 2.0], 1),
+        4.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
     assert isclose(
-        partial_derivative(fn, [0., 2.], 0),
-        0.,
+        partial_derivative(fn, [0.0, 2.0], 0),
+        0.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
 
@@ -53,7 +53,31 @@ def test_partial_derivative_with_numpy_arrays():
     fn = lambda x, y: x ** 2 + y ** 2
 
     assert isclose(
-        partial_derivative(fn, np.array([1., 2.]), 1),
-        4.,
+        partial_derivative(fn, np.array([1.0, 2.0]), 1),
+        4.0,
         abs_tol=DIFFERENTIATION_STEP,
     )
+
+
+def test_gradient_paraboloid():
+    fn = lambda x, y: x ** 2 + y ** 2
+    actual = gradient(fn, [1.0, 2.0])
+    expected = [2.0, 4.0]
+
+    assert type(actual) == type(expected)
+    assert len(actual) == len(expected)
+
+    for actual_num, expected_num in zip(actual, expected):
+        assert isclose(actual_num, expected_num, abs_tol=DIFFERENTIATION_STEP)
+
+
+def test_gradient_paraboloid_with_numpy_arrays():
+    fn = lambda x, y: x ** 2 + y ** 2
+    actual = gradient(fn, np.array([1.0, 2.0]))
+    expected = np.array([2.0, 4.0])
+
+    assert type(actual) == type(expected)
+    assert len(actual) == len(expected)
+
+    for actual_num, expected_num in zip(actual, expected):
+        assert isclose(actual_num, expected_num, abs_tol=DIFFERENTIATION_STEP)

8234a91aa4cdc2953da9a2dc8b43c7b7b23f3c4b
Sergei <sergey.2123@yandex.ru>
2021-05-15 12:24:17 +0300

    Seidel method added

---
+++ lab3/primathlab3/iteration_method.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# ПУНКТ 3
+# Методы решения СЛАУ Зейделя
+def seidel_method(A, b, eps):
+    """Найти решение системы Ax=b методом Гаусса-Зейделя
+    A - матрица, хранящаяся в разреженном виде
+    b - вектор в правой части
+    returns:
+    None - если решения не существует
+    x - решение
+    """
+    n = len(A)
+    x = np.zeros(n)
+
+    converge = False
+    while not converge:
+        x_new = np.copy(x)
+        for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in ranger(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+
+        converge = np.sqrt(sum((x_new[i] - x[i]) ** 2 for i in range(n))) <= eps
+        x = x_new
+
+    return x
+
+# test_solving_function(seidel_method)

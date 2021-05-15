32f9bddc52672ff7d9e4371022ea91ca7eb9a4d0
Ilya Lunev <ilunev@protonmail.com>
2021-05-15 16:43:50 +0300

    Добавил тест Зейделя

--- lab3/tests/test_iteration.py
+++ lab3/tests/test_iteration.py
@@ -8,18 +8,12 @@ from scipy.sparse import csr_matrix
     ["matrix", "vector", "expected_answer"],
     [
         (
-            [[5, 7, 4], [9, 5, 7], [1, 2, 7]],
-            [4, 5, 2],
-            [63.0 / 235, 67.0 / 235, 39.0 / 235],
-        ),
-        (
-            [[7, 7, 30], [4, 7, 9], [7, 1, 30]],
-            [-6, 6, 3],
-            [303.0 / 38, -3.0 / 2, -65.0 / 38],
+            [[5, 1, 1], [1, 6, 2], [0, 1, 7]],
+            [7, 13, 2],
+            [1, 2, 0],
         ),
     ],
 )
-
 def test_seidel_method(matrix, vector, expected_answer):
     matrix = csr_matrix(matrix)
     actual_answer = seidel_method(matrix, vector, 10e-3)

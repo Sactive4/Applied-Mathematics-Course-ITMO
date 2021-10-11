eb1b087a3aa8136325d7216ce98f75fe51395791
Ilya Lunev <ilunev@protonmail.com>
2021-10-11 23:33:09 +0300

    Работа за 11 октября
    Co-authored-by: George Semenov <MrGeorgeous@users.noreply.github.com>

---
+++ lab4/table.py
@@ -0,0 +1,112 @@
+import numpy as np
+
+from task import Task, ConstraintSign
+
+
+class Table:
+    # n начальных переменных
+    # m ограничений (среди них k неравенств, это для новых переменных нужно)
+    # то
+
+    # таблица
+    #      |  b  |   1  |   2   |   3  |   4   |   5  |   6  |   7
+    #   5  |  b1     3      1       1       1      1      0      1
+    #   6  |  b2                                         
+    #   7  |  b3                                              
+    #   K  |  0     с_1 * x_1  c_2 * x_2 ...
+
+    # x_0 (json: start и f): x_0 * f
+
+    # строк будет (m + 1), последняя для K
+    # столбцов будет (1 + (n + k - m)), 
+    # тогда адресация будет прямая, например, [0][1], для пересечения переменной 1 и 4
+
+    # будет отдельный массив rows, columns, который будет сопоставлять каждому строке/столбцу номер переменной
+
+    def real_init(self, task: Task, start=None):
+        start = start or []
+        self.n = len(task.f)
+        self.m = len(task.constraints)
+        self.table = ...
+
+
+    def __init__(self, task: Task):
+        self.n = 2 # количество начальных переменных
+        self.k = 3 # количество неравенств
+        self.m = 3 # количество ограничений
+        self.table = np.zeros(shape=(self.m + 1, self.n + self.k - self.m)) # double
+        self.v = np.zeros(shape=(self.n + self.k))
+        self.rows = np.empty(shape=(self.m + 1)) # int
+        self.columns = np.empty(shape=(self.n + self.k - self.m)) # int
+
+        self.table = np.array([ [4, 2, -1, 1, 0, 0], [2, 1, -2, 0, 1, 0], [5, 1, 1, 0, 0, 1], [0, -3, 1, 0, 0, 0]], dtype=np.double)
+        self.table[:, 1:] *= (-1)
+        self.v[self.m:] = 1
+        self.rows = np.array([2, 3, 4])
+        self.columns = np.array([0, 1, 2, 3, 4])
+
+    def swap(row, column):
+        """
+        Меняет местами ...
+        @param row: номер строки (0..m-1)
+        @param column: номер столбца (0..(n+k-m-1))
+        """
+        ...
+
+    def __rectangle_value(self, a, x):
+        return self.table[x[0], x[1]] - (self.table[a[0], x[1]] * self.table[x[0], a[1]]) / self.table[a[0], a[1]]
+
+    def solve(self):
+        N = 100 # max iterations
+        for i in range(N):
+            x = self.next_step()
+            if x is None:
+                continue
+            else:
+                return x
+        return []
+
+    def next_step(self):
+
+        print(self.table)
+
+        # индекс разрешающего столбца
+        j = 1 + np.argmin(self.table[-1, 1:])
+        if self.table[-1, j] >= 0:
+            # минимум найден, останавливаемся
+            return self.v[:(self.n)]
+        
+        # индекс разрешающей строки
+        i = np.argmin( self.table[:-1, 0] / self.table[:-1, j])
+        #print((i,j))
+        #print(self.table[i, j])
+        if self.table[i, j] >= 0:
+            # решения нет
+            return []
+
+        new_table = np.array(self.table, copy=True)
+        for x in range(self.table.shape[0]):
+            for y in range(self.table.shape[1]):
+                if x == i:
+                    new_table[x, y] /= self.table[i, j]
+                    if y != j:
+                        new_table[x, y] *= (-1)
+                else:
+                    new_table[x, y] = self.__rectangle_value((i, j), (x, y))
+        
+        self.table[:] = new_table # np.array(new_table, copy=True)
+        self.rows[i] = self.columns[j - 1]
+        self.v = np.zeros(shape=(self.v.shape[0]))
+        for x in range(len(self.rows)):
+            self.v[self.rows[x]] = self.table[x, 0]
+
+        #print(self.table)
+        #print(self.table[:-1, :(self.m+1)])
+        #print(np.linalg.solve(self.table[:-1, :(self.m+1)], self.table[:, 0]))
+        #print(self.table)
+
+        return None
+
+
+t = Table(None)
+print(t.solve())
--- lab4/task.py
+++ lab4/task.py
@@ -1,30 +1,141 @@
 # Task solution
 import json
 
-import numpy as np
-import scipy.optimize
+# import numpy as np
+# import scipy.optimize
 
-class Task:
+from pydantic import BaseModel
+from enum import Enum
+from math import isclose
 
-    f = np.array([])
-    c = np.array([])
 
-    '''
-    Инициализация в канонической форме
-    param f: минимизируемая линейная функция (столбик коэффициентов перед x)
-    param c: 2-мерная матрица ограничений в форме <=
-    Все переменные принимаются x_j >= 0
-    '''
-    def __init__(self, f, c):
-        self.f = np.array(f)
-        self.c = np.array(c)
+class TaskType(str, Enum):
+    min = "min"
+    max = "max"
 
-    def __init__(self, filename):
-        f = open(filename)
-        d = json.load(f)
-        self.f = np.array(d['f'])
-        self.c = d['c']
-        f.close()
 
+class ConstraintSign(str, Enum):
+    le = "<="
+    ge = ">="
+    eq = "="
+
+
+    def reverse(self):
+        if self is ConstraintSign.le:
+            return ConstraintSign.ge
+        elif self is ConstraintSign.ge:
+            return ConstraintSign.le
+        return self
+
+
+class Constraint(BaseModel):
+    a: list[float]
+    sign: ConstraintSign
+    b: float
+
+
+    def mul_in_place(self, k):
+        self.a = [x * k for x in self.a]
+        self.b *= k
+
+        if k < 0:
+            self.sign = self.sign.reverse()
+
+
+class Task(BaseModel):
+    type: TaskType
+    f: list[float]
+    constraints: list[Constraint]
+    # start: list[float]
+
+
+    @staticmethod
+    def load(filename):
+        with open(filename) as f:
+            data = json.load(f)
+            return Task(**data)
+    
+
+    def to_canonical(self):
+        task = self.copy()
+
+        # Задачу максимизации приводим к задаче минимизации
+        # путём умножения функции на -1
+
+        if task.type is TaskType.max:
+            task.f = [-x for x in task.f]
+            task.type = TaskType.min
+
+        # Ограничения >= приведём к ограничениям <= путём
+        # умножения их на -1
+
+        for c in task.constraints:
+            if c.sign is ConstraintSign.ge:
+                c.mul_in_place(-1)
+
+        # Неравенства приведём к равенствам путём введения
+        # новых переменных
+
+        for c in task.constraints:
+            if c.sign is ConstraintSign.eq:
+                continue
+
+            task.f.append(0.)
+            c.a.append(1.)
+
+            for c_ in task.constraints:
+                if c_ is not c:
+                    c_.a.append(0.)
+            
+            c.sign = ConstraintSign.eq
+        
+        # Сделаем правые части равенств положительными путём
+        # умножения равенств на -1
+
+        for c in task.constraints:
+            if c.b < 0:
+                c.mul_in_place(-1)
+        
+        # TODO: остальные шаги приведения в канонический вид (?)
+        return task
+    
+
+    @staticmethod
+    def _pretty_summands(coefs):
+        return " + ".join(
+            f"{c}x{i}"
+            for i, c in enumerate(coefs)
+            if not isclose(c, 0.)
+        )
+    
+
+    def __str__(self):
+        lines = []
+        fn_line = "f(x) = " + self._pretty_summands(self.f)
+        lines.append(fn_line)
+
+        for c in self.constraints:
+            line = self._pretty_summands(c.a) + " " + c.sign + " " + str(c.b)
+            lines.append(line)
+        
+        return "\n".join(lines)
+
+    
     def solve_scipy(self):
-        return scipy.optimize.linprog(c=self.c, A_eq=self.c[:, 0:-1], b_eq=self.c[:, -1])
+        # TODO
+        # self.f: минимизируемая линейная функция (столбик коэффициентов перед x)
+        # self.c: 2-мерная матрица ограничений в форме <=
+        # Все переменные принимаются x_j >= 0
+        # return scipy.optimize.linprog(c=self.f, A_eq=self.c[:, 0:-1], b_eq=self.c[:, -1])
+        
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    import sys
+    task = Task.load(sys.argv[1])
+    print("=== Исходная задача ===")
+    print(task)
+    canonical = task.to_canonical()
+    print("=== Канонический вид ===")
+    print(canonical)
